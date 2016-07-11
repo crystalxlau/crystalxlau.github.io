d3c6dd236aaa55aa4692cfc4d658fb9b8ba445c5
crystalxlau <crystal.lau11@gmail.com>
2016-07-11 07:58:03 -0400

    completed carousel rotation

--- js/index.js
+++ js/index.js
@@ -10,17 +10,14 @@ function initialize(){
     {name: 'Ivan Ramen', address: '25 CLINTON STREET, NEW YORK, NY 10002', lat: 40.720780, lng: -73.984529},
     {name: 'Kuma Inn', address: '113 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.719783, lng: -73.988904},
     {name: 'Los Feliz', address: '109 LUDLOW STREET #1, NEW YORK, NY 10002', lat: 40.719640, lng: -73.988922},
-    {name: 'Lam Zhou Handmade Noodle', address: '144 EAST BROADWAY, NEW YORK, NY 10002', lat: 40.714345, lng: -73.99213},
+    {name: 'Cafe Katja', address: '79 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
     {name: 'Pig and Khao', address: '68 CLINTON STERET, NEW YORK, NY 10002', lat: 40.719314, lng: -73.984793},
-    {name: 'Clinton Street Baking Company', address: '4 CLINTON STREET, NEW YORK, NY 10002', lat: 40.721199, lng: -73.983823},
+    {name: 'Ice & Vice', address: '221 EAST BROADWAY, NEW YORK, NY 10002', lat: 40.714288, lng: -73.986981},
     {name: 'The Meatball Shop', address: '84 STANTON STREET, 10002', lat: 40.721668, lng: -73.988781},
     {name: 'Dudleys', address: '85 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.717972, lng: -73.990456},
     {name: 'Doughnut Plant', address: '379 GRAND STREET, NEW YORK, NY 10002', lat: 40.716249, lng: 73.989941},
-    {name: 'The Fat Radish', address: '17 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
+    {name: 'Fat Radish', address: '17 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
     {name: 'Dirty French', address: '180 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
-    {name: 'Melt Bakery', address: '132 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
-    {name: 'Prohibition Bakery', address: '9 CLINTON STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
-    {name: 'Il Laboratorio del Gelato', address: '188 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.722262, lng: -73.987045},
   ];
 
   locations.forEach(function(element, index, array){
@@ -59,6 +56,30 @@ function initialize(){
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+//****SPIN ON CLICK*****/
+var spin= document.querySelector(".spin")
+  spin.addEventListener("click", spinWheel);
+
+var pickerOne = (Math.floor(Math.random()*12))
+var stopAngleOne = 720+pickerOne*30+"deg"
+var rotateValueOne = "rotateX(-"+stopAngleOne+")"
+
+var ringOne = document.querySelector("#wheel")
+
+function spinWheel() {
+  ringOne.style.transform = rotateValueOne;
+}
+
+var reset = document.querySelector("#reset")
+  reset.addEventListener("click", reload);
+
+function reload (){
+  window.location.reload(false);
+}
+
+
 
 
 
