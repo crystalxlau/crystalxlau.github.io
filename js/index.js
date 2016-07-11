f7eba2e4b24f314b54397abd0d6f8935b23ba742
crystalxlau <crystal.lau11@gmail.com>
2016-07-11 19:45:07 -0400

    fixed marker positions

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function initialize(){
   var mapOptions = {
-    zoom: 14,
+    zoom: 15,
     center: {lat: 40.718667, lng: -73.985261}
-  };
+  },
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
   var locations = [
@@ -10,14 +10,14 @@ function initialize(){
     {name: 'Ivan Ramen', address: '25 CLINTON STREET, NEW YORK, NY 10002', lat: 40.720780, lng: -73.984529},
     {name: 'Kuma Inn', address: '113 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.719783, lng: -73.988904},
     {name: 'Los Feliz', address: '109 LUDLOW STREET #1, NEW YORK, NY 10002', lat: 40.719640, lng: -73.988922},
-    {name: 'Cafe Katja', address: '79 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
+    {name: 'Cafe Katja', address: '79 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.717832, lng: -73.990585},
     {name: 'Pig and Khao', address: '68 CLINTON STERET, NEW YORK, NY 10002', lat: 40.719314, lng: -73.984793},
     {name: 'Ice & Vice', address: '221 EAST BROADWAY, NEW YORK, NY 10002', lat: 40.714288, lng: -73.986981},
     {name: 'The Meatball Shop', address: '84 STANTON STREET, 10002', lat: 40.721668, lng: -73.988781},
     {name: 'Dudleys', address: '85 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.717972, lng: -73.990456},
-    {name: 'Doughnut Plant', address: '379 GRAND STREET, NEW YORK, NY 10002', lat: 40.716249, lng: 73.989941},
-    {name: 'Fat Radish', address: '17 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
-    {name: 'Dirty French', address: '180 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.730610, lng: -73.935242},
+    {name: 'Doughnut Plant', address: '379 GRAND STREET, NEW YORK, NY 10002', lat: 40.716334, lng: -73.988649},
+    {name: 'Fat Radish', address: '17 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.715360, lng: -73.991846},
+    {name: 'Dirty French', address: '180 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.722109, lng: -73.987274},
   ];
 
   locations.forEach(function(element, index, array){
@@ -72,13 +72,14 @@ function spinWheel() {
   ringOne.style.transform = rotateValueOne;
 }
 
+/******RESET WHEEL******/
+
 var reset = document.querySelector("#reset")
   reset.addEventListener("click", reload);
 
 function reload (){
   window.location.reload(false);
 }
-
 
 
 
