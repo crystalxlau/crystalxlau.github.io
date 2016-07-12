355cdce19f79996662b8e2f8763a73a48cdbd24d
crystalxlau <crystal.lau11@gmail.com>
2016-07-12 10:33:12 -0400

    added styles

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
-function initialize(){
-  var mapOptions = {
-    zoom: 15,
-    center: {lat: 40.718667, lng: -73.985261}
-  },
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  var locations = [
+/****RESTAURANT LOCATIONS****/
+var locations = [
     {name: 'Casa Mezcal', address: '86 ORCHARD STREET, NEW YORK, NY 10002', lat: 40.718030, lng: -73.990120},
     {name: 'Ivan Ramen', address: '25 CLINTON STREET, NEW YORK, NY 10002', lat: 40.720780, lng: -73.984529},
     {name: 'Kuma Inn', address: '113 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.719783, lng: -73.988904},
@@ -20,25 +14,35 @@ function initialize(){
     {name: 'Dirty French', address: '180 LUDLOW STREET, NEW YORK, NY 10002', lat: 40.722109, lng: -73.987274},
   ];
 
+/****LOAD MAP****/
+function initialize(locations){
+  var mapOptions = {
+    zoom: 15,
+    center: {lat: 40.718667, lng: -73.985261}
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+/****ADD LOCATION MARKERS****/
   locations.forEach(function(element, index, array){
     var marker, content;
 
-    marker = createMarker(element);
-    content = createInfoWindow(element, marker);
+    marker = createMarker(element, index);
+    createInfoWindow(element, marker);
   });
 
-  function createMarker(element){
+  function createMarker(element, index){
     var coordinates = new google.maps.LatLng(element.lat, element.lng); 
 
     var marker = new google.maps.Marker({
       position: coordinates,
       map: map,
-      title: element.name
     });
 
     return marker;
   }
 
+/****ADD INFO WINDOW****/
   function createInfoWindow(element, marker){
     var contentString;
 
@@ -49,37 +53,47 @@ function initialize(){
       maxWidth: 150
     });
 
-    google.maps.event.addListener(marker, 'click', function(){
+    marker.addListener('click', function(){
+      console.log('marker was clicked');
       infoWindow.open(map, marker);
     });
   }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+/****LOAD BLANK MAP****/
+google.maps.event.addDomListener(window, 'load', initialize([]));
 
 
-//****SPIN ON CLICK*****/
+//****SPIN ON CLICK****/
 var spin= document.querySelector(".spin")
-  spin.addEventListener("click", spinWheel);
+spin.addEventListener("click", spinWheel);
 
-var pickerOne = (Math.floor(Math.random()*12))
-var stopAngleOne = 720+pickerOne*30+"deg"
+var selectedLocationIndex = (Math.floor(Math.random()*12))
+var stopAngleOne = 720+selectedLocationIndex*30+"deg"
 var rotateValueOne = "rotateX(-"+stopAngleOne+")"
 
 var ringOne = document.querySelector("#wheel")
 
 function spinWheel() {
   ringOne.style.transform = rotateValueOne;
+  window.setTimeout(filterLocations, 4000);
 }
 
-/******RESET WHEEL******/
+function filterLocations() {
+  var selectedLocation = locations[selectedLocationIndex]
+  initialize([selectedLocation]);
+}
 
+/****RESET WHEEL****/
 var reset = document.querySelector("#reset")
   reset.addEventListener("click", reload);
 
 function reload (){
   window.location.reload(false);
 }
+
+
+
 
 
 
